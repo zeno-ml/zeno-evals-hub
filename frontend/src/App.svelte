4c2f8ad910bbb5606ca70b90d121b5754e8b325e
stevenyh3 <89871868+stevenyh3@users.noreply.github.com>
2023-04-19 14:45:32 -0400

    feat: add initial design

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,30 +5,69 @@
 <main>
   <h1>OpenAI Evals Hub</h1>
   <h3>
-    Explore the results of OpenAI evals using <a href="https://zenoml.com">Zeno</a>
+    Explore the results of OpenAI evals using
+    <a href="https://zenoml.com">Zeno</a>
   </h3>
-
   <!-- table with links to zeno sites. -->
-  <table>
-    <thead>
-      <tr>
-        <th>Evaluation</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#await data}
-        <p>loading</p>
-      {:then final_data}
-        {#each final_data as d}
-          {@const name = Array.from(Object.keys(d))[0]}
-          <tr>
-            <td><a href="/{name}/">{name}</a></td>
-          </tr>
-        {/each}
-      {/await}
-    </tbody>
-  </table>
+  <div id="container">
+    <!-- table with links to zeno sites. -->
+    <table>
+      <thead>
+        <tr>
+          <th>Evaluation</th>
+          <th>View</th>
+          <th>Models</th>
+          <th>Accuracy</th>
+          <th>data_column</th>
+          <th>id_column</th>
+          <th>batch_size</th>
+          <th>samples</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#await data}
+          <p>loading</p>
+        {:then final_data}
+          {#each final_data as d}
+            {@const name = Object.keys(d)[0]}
+            {@const zeno = d[name]["zeno"]}
+            {@const spec = d[name]["spec"]}
+            <tr>
+              <td><a href="/{name}/">{name}</a></td>
+              <td>{zeno["view"]}</td>
+              <td>{zeno["models"][0]}<br /> {zeno["models"][1]}</td>
+              <td>{spec["accuracy"][0]}<br />{spec["accuracy"][1]}</td>
+              <td>{zeno["data_column"]}</td>
+              <td>{zeno["id_column"]}</td>
+              <td>{zeno["batch_size"]}</td>
+              <td>{zeno["samples"]}</td>
+            </tr>
+          {/each}
+        {/await}
+      </tbody>
+    </table>
+  </div>
 </main>
 
 <style>
+  #container {
+    margin: 50px 20px;
+    display: flex;
+    justify-content: center;
+  }
+  table {
+    border-collapse: collapse;
+    text-align: left;
+  }
+  table thead tr th {
+    border-bottom: 0.5px solid grey;
+  }
+  table th,
+  table td {
+    border-bottom: 0.5px solid grey;
+  }
+  table th,
+  table td {
+    padding: 4px 20px;
+  }
 </style>
--- zeno-evals-hub/main.py
+++ zeno-evals-hub/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 
@@ -5,14 +6,74 @@ import uvicorn
 import yaml  # type: ignore
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from zeno import get_server, zeno  # type: ignore
+from zeno import get_server, zeno, ZenoParameters  # type: ignore
 from zeno_evals import generate_zeno_config  # type: ignore
+
+
+# parse information in spec
+def prepare_spec(params, second_exists):
+    res = {}
+
+    data = []
+    with open(params["results-file"]) as f:
+        for index, line in enumerate(f):
+            data.append(json.loads(line))
+            if index == 1:
+                break
+    data2 = []
+    if second_exists:
+        with open(params["second-results-file"]) as f:
+            for index, line in enumerate(f):
+                data2.append(json.loads(line))
+                if index == 1:
+                    break
+
+    res["accuracy"] = [
+        data[1]["final_report"]["accuracy"],
+        data2[1]["final_report"]["accuracy"] if second_exists else "",
+    ]
+    return res
+
+
+def prepare_zeno_params(config: ZenoParameters):
+    res = {}
+    res["models"] = config.models
+    res["view"] = config.view
+    res["data_column"] = config.data_column
+    res["id_column"] = config.id_column
+    res["batch_size"] = config.batch_size
+    res["samples"] = config.samples
+    return res
+
+
+# handle not having a second results or functions file
+def prepare_zeno_config(params, second_exits, function_exists) -> ZenoParameters:
+    if second_exits and function_exists:
+        return generate_zeno_config(
+            params["results-file"],
+            params["second-results-file"],
+            params["functions-file"],
+        )
+    elif second_exits:
+        return generate_zeno_config(
+            params["results-file"],
+            params["second-results-file"],
+        )
+    elif function_exists:
+        return generate_zeno_config(
+            params["results-file"],
+            params["functions-file"],
+        )
+    else:
+        return generate_zeno_config(
+            params["results-file"],
+        )
 
 
 def command_line():
     app = FastAPI(title="Frontend API")
-
     args = []
+
     with open(sys.argv[1], "r") as f:
         args = yaml.safe_load(f)
 
@@ -26,12 +87,11 @@ def command_line():
     for entry in args:
         name = list(entry.keys())[0]
         params = entry[name]
-        # TODO: handle not having a second results or functions file
-        config = generate_zeno_config(
-            params["results-file"],
-            params["second-results-file"],
-            params["functions-file"],
-        )
+        second_exists = True if "second-results-file" in params else False
+        function_exists = True if "function-results" in params else False
+        config = prepare_zeno_config(params, second_exists, function_exists)
+        params["spec"] = prepare_spec(params, second_exists)
+        params["zeno"] = prepare_zeno_params(config)
         config.serve = False
         zeno_obj = zeno(config)
         if zeno_obj is None:
